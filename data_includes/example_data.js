988ff03e5e432ddcfbef7e2f3c12f59cdff77563
Silvia <31701368+SilviaPag95@users.noreply.github.com>
2019-02-12 19:47:22 +0100

    Add files from old pilot

---
+++ data_includes/example_data.js
@@ -0,0 +1,69 @@
+var shuffleSequence = seq("intro", "instructions", sepWith("sep", rshuffle(startsWith("question"))));
+var practiceItemTypes = ["practice"];
+
+var defaults = [
+    "Separator", {
+        transfer: 1000,
+        normalMessage: "Please wait for the next question."
+    },
+    "DashedSentence", {
+        mode: "self-paced reading"
+    },
+    "Message", {
+        hideProgressBar: true
+    },
+    "Form", {
+        hideProgressBar: true,
+        continueOnReturn: false,
+        saveReactionTime: false
+    },
+];
+
+var items = [
+
+    ["sep", "Separator", { }],
+
+    ["intro", "Form", {
+        html: { include: "example_intro.html" },
+        validators: {
+            age: function (s) { if (s.match(/^\d+$/)) return true; else return "Bad value for \u2018age\u2019"; }
+        }
+    } ],
+    ["instructions", "Form", {
+        html: {include: "instructions.html" },
+        }
+    ],
+                
+    ["question1", "FormBIS", {
+        html: { include: "Page1.html" },
+    } ],
+    
+    ["question2", "FormBIS", {
+        html: { include: "Page2.html" },
+    } ],
+    
+    ["question3", "FormBIS", {
+        html: { include: "Page3.html" },
+    } ],
+    
+    ["question4", "FormBIS", {
+        html: { include: "Page4.html" },
+    } ],
+    
+    ["question5", "FormBIS", {
+        html: { include: "Page5.html" },
+    } ],
+    
+    ["question6", "FormBIS", {
+        html: { include: "Page6.html" },
+    } ],
+    
+    ["question7", "FormBIS", {
+        html: { include: "Page7.html" },
+    } ],
+
+    ["question8", "FormBIS", {
+        html: { include: "Page8.html" },
+    } ]
+             
+];
